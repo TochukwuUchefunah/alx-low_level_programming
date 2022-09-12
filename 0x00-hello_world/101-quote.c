0f0f7e6ba99ba5b005fc1f5e8977b85d173223ae
Tochukwu Uchefunah <tuchefuna@gmail.com>
2022-09-12 09:56:14 -0700

    one more attempt

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,11 +2,11 @@
 /**
  * main - Entry point
  *
- * Return: 0 
+ * Return: 1
  */
-int main(void)
+int main (void)
 {
 
 	fwrite(2, "and that piece of art is useful" - "Dora Korpar,2015-10-19\n", 61);
-	return (0);
+	return (1);
 }
